efd2d1bb930bada8d500874faf8e945ca3b0fc6e
Sergio Moreno <21252@columbushs.com>
2019-12-09 10:24:49 -0500

    in p[rogress

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,35 @@
 // Soldier
-class Soldier {}
-
-// Viking
-class Viking {}
-
-// Saxon
-class Saxon {}
-
-// War
-class War {}
+class Soldier {
+    constructor(health, strength) {
+     this.health = health
+     this.strength = strength
+    }
+    attack() {
+        return this.strength
+    }
+    receiveDamage() {
+        
+        this.health = this.health - damage
+    }
+  }
+  
+  // Viking
+  class Viking {
+    constructor(name, health, strength) {
+        this.name = name
+        this.health = health
+        this.strength = strength
+    }
+    receiveDamage() {
+        this.health = this.health - damage
+        if(this.health > 0) {
+            return '$'
+        }
+    }
+  }
+  
+  // Saxon
+  class Saxon {}
+  
+  // War
+  class War {}
